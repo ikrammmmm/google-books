859e4d66399f54d2909b331f1433366a093189a6
ikrammmmm <ikramhussein1234@gmail.com>
2021-06-08 10:47:15 +0530

    code cleanup

--- client/src/helpers/api_helper.js
+++ client/src/helpers/api_helper.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+require("dotenv").config()
 
 var appPort = process.env.PORT || 3000;
 export  function getBooks(searchText)
 {
-    return axios.get("https://www.googleapis.com/books/v1/volumes?q="+searchText+"&maxResults=20&key="+process.env.API_KEY);
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q="+searchText+"&maxResults=20&key=AIzaSyCf-hE4wospNNs_NCIQOlX2LceaXcWYgdE");
 }
 export async function saveBook(book)
 {
